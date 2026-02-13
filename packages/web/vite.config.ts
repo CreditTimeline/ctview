7a3b5a659317be7a8e5f4bdff16d7c6d6b1d4e85
Rob Knight <12484127+rknightion@users.noreply.github.com>
2026-02-13 13:51:51 +0000

    ci: fix

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  ssr: {
+    external: ['ajv', 'ajv-formats', 'better-sqlite3'],
+  },
 });
